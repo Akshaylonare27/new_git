ca1102d5540158c6b039ba2969e0fe86329641a8
unknown <akshay23lonare@gmail.com>
2022-09-14 23:43:34 +0530

    check prime

---
+++ prime.js
@@ -0,0 +1,14 @@
+for (let i=1;i<=20;i++){
+    let count=0;
+    for (let j=1;j<i;j++){
+      if(i%j==0){
+        
+      }
+    }
+  
+    if(count==2){
+      console.log(i,"prime");
+    }else {
+      console.log(i,"Not a Prime")
+    }
+  }
